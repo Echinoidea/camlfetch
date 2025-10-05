71bb356f517636da13221c62eda25a95346d36c0
Gabriel Hooks <gabriel.i.hooks@gmail.com>
2025-10-05 15:30:19 -0400

    Fix config parse

--- bin/main.ml
+++ bin/main.ml
@@ -3,14 +3,23 @@ open Camlfetch
 let art = Ascii.read_art "/home/gabriel/.local/share/fastfetch/ascii/arch.txt"
 
 let conf =
-  match Config.load_from_file "/home/gabriel/.config/camlfetch.sexp" with
+  match
+    Config.load_from_file "/home/gabriel/.config/camlfetch/camlfetch.sexp"
+  with
   | Ok cfg ->
+      Printf.eprintf "Loaded config from file\n%!" ;
       cfg
-  | Error _ ->
+  | Error (`File_error msg) ->
+      Printf.eprintf "Warning: Could not read config file: %s\n%!" msg ;
+      Printf.eprintf "Using default configuration\n%!" ;
+      Config.default
+  | Error (`Parse_error msg) ->
+      Printf.eprintf "Warning: Could not parse config file: %s\n%!" msg ;
+      Printf.eprintf "Using default configuration\n%!" ;
       Config.default
 
-(*I want a function that takes a bool from conf, a field title, a field value, and index *)
-(* This will iterate over all potential fields, and only return a string with content if cfg_includes *)
+(*I want a function that takes a bool from conf, a field title, a field v
+all potential fields, and only return a string with content if cfg_includes *)
 let field cfg_includes formatted_string =
   if cfg_includes then formatted_string else ""
 
@@ -31,6 +40,8 @@ let fields =
   ; field conf.show_uptime (Printf.sprintf "UPTIME\t%d:%d" hours minutes)
   ; field conf.show_ocaml (Printf.sprintf "OCAML\t%s" Ocaml.ocaml_version)
   ; field conf.show_palette (Printf.sprintf "%s" (Palette.palette "*")) ]
+
+let fields = List.filter (fun s -> s <> "") fields
 
 let sysinfo_s = String.concat "\n" fields
 
